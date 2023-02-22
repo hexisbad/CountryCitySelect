c9c7ce9c3833df54e625c8c678b0090bf942128f
hexisbad <advaithkometh@gmail.com>
2023-02-22 21:54:57 +0400

    Added CSS changes, fixed the cities selector(no cities just states, API limitation)

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,68 +3,69 @@ import './App.css';
 
 const TokenHeaders = {
   Accept: 'application/json',
-  'api-token':
-    'ocX0Beotv18nujCadTsubyCRc6PX_5i7l0PGrsEM54ZP7k6ZOTfls0_hHzB7bCWi04s',
+  'api-token':  'ocX0Beotv18nujCadTsubyCRc6PX_5i7l0PGrsEM54ZP7k6ZOTfls0_hHzB7bCWi04s',
   'user-email': 'advaithkometh@gmail.com',
 };
 
 function App() {
-  const cities = [
-    { id: '1', countryId: '1', name: 'Dubai' },
-    { id: '2', countryId: '1', name: 'Al Ain' },
-    { id: '3', countryId: '2', name: 'Mumbai' },
-    { id: '4', countryId: '2', name: 'Cochin' },
-    { id: '5', countryId: '3', name: 'Portugese city 1' },
-    { id: '6', countryId: '3', name: 'Portugese city 2' },
-  ];
 
   const [countries, setCountries] = useState([]);
   const [token, setToken] = useState(null);
-  const [city, setCity] = useState([]);
+  const [country, setCountry] = useState('');
+  const [cities, setCities] = useState('');
+  const [buttonText, setButtonText] = useState('Send it home')
 
   useEffect(() => {
     fetch('https://www.universal-tutorial.com/api/getaccesstoken', {
-      method: 'GET',
-      headers: TokenHeaders,
+      headers: TokenHeaders
     })
-      .then((r) => r.json())
-      .then((d) => setToken(d));
+      .then(r => r.json())
+      .then(d => setToken(d));
   }, []);
 
   useEffect(() => {
     if (token) {
       fetch('https://www.universal-tutorial.com/api/countries/', {
-        method: 'GET',
         headers: {
           Authorization: `Bearer ` + token.auth_token,
-          Accept: 'application/json',
+          Accept: 'application/json'
         },
       })
-        .then((r) => r.json())
-        .then((d) => setCountries(d));
+        .then(result => result.json())
+        .then(data => setCountries(data));
     }
   }, [token]);
 
   useEffect(() => {
-    if (countries) {
-      console.log('fucking finally some countries', countries);
+    if (country && token) {
+      fetch(`https://www.universal-tutorial.com/api/states/${country}`, {
+        headers: {
+          Authorization: `Bearer ` + token.auth_token,
+          Accept: 'application/json'
+        },
+     })
+      .then(result => result.json())
+      .then(data => setCities(data))
     }
-  }, [countries]);
+  }, [country]);
 
-  const handleCity = (id) => {
-    const dt = cities.filter((city) => city.countryId === id);
-    setCity(dt);
+  const handleCountry = (country) => {
+    setCountry(country);
+  };
+
+  const onButtonPress = () => {
+    setButtonText('Psych')
   };
 
   return (
     <div className='App'>
       <header className='App-header'>
-        <h2>Advaith's Country-City Selector</h2>
+        <h2>Advaith's Country <strike>City</strike> State Selector</h2>
       </header>
       <div className='user-container '>
         <select
-          className='form-control select-class'
-          onChange={(event) => handleCity(event.target.value)}
+          className='form-control'
+          onChange={(event) => handleCountry(event.target.value)}
         >
           <option className='info-item' value='0'>
             {' '}
@@ -83,24 +84,23 @@ function App() {
         <br></br>
         <select
           id='cities'
-          className='form-control select-class'
-          // onChange={(event) => handleCity(event.target.value)}
+          className='form-control'
         >
           <option className='info-item' value='0'>
             {' '}
-            Select City{' '}
+            Select State{' '}
           </option>
-          {city && city !== undefined
-            ? city.map((city, index) => {
+          {cities && cities !== undefined
+            ? cities.map((city, index) => {
                 return (
                   <option className='info-item' key={index} value={city.id}>
-                    {city.name}
+                    {city.state_name}
                   </option>
                 );
               })
             : 'No City'}
         </select>
-        <button className='button-52'>Send it home</button>
+        <button onClick={onButtonPress} className='button-52'>{buttonText}</button>
       </div>
     </div>
   );
